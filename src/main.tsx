f455a9f101fb629f946e36cb409ba20bb15a8332
LonerLykos <loner.lykos.project@gmail.com>
2025-01-14 18:23:15 +0200

    update master

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(<App/>)
+let routes = createBrowserRouter([
+    {path: '/', element: <App/>}
+]);
+
+createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>)
